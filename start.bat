71a9082b879df3900a922fc4ca0db4465d1a165c
Caossns <86632945+Caossns@users.noreply.github.com>
2021-07-01 00:31:23 +0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Kentav" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user kanetav KentavGans920 /add >nul
+net localgroup administrators kanetav /add >nul
+net user kanetav /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant kanetav:F >nul
+ICACLS C:\Windows\installer /grant kanetav:F >nul
+echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+echo Username: kanetav
+echo Password: KentavGans920
+echo Silahkan Login Ke RDP Anda Sekarang
+ping -n 10 127.0.0.1 >nul
